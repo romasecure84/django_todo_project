dc980023f525fdcd690620b56187559c3c83f11e
Rafail Jabrayilov <romasecure84@yandex.ru>
2024-01-20 21:32:01 +0400

    footer ve navbar elave edildi

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from todo.models import Todo
+# from django.http import HttpResponse
 
 def home_view(request):
-    return render(request, 'todo/todo_list.html', {})
+    todos = Todo.objects.filter(is_active=True)
+    context = dict(
+        todos=todos
+    )
+    return render(request, 'todo/todo_list.html', context)
