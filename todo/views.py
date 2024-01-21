71b08f200fd7f9adf32960b558d67a508f05fb9e
Rafail Jabrayilov <romasecure84@yandex.ru>
2024-01-21 22:35:06 +0400

    css ve js sehv verdi, ona gore de link istifadesine kecirildi

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from todo.models import Todo
-from django.http import Http404
+from django.shortcuts import get_object_or_404
 
 def home_view(request):
     todos = Todo.objects.filter(
@@ -11,12 +11,20 @@ def home_view(request):
     )
     return render(request, 'todo/todo_list.html', context)
 
+# def todo_detail_view(request, id):
+#     try:
+#         todo = Todo.objects.get(pk=id)
+#         context = dict(
+#           todo=todo
+#           )
+#         return render(request, 'todo/todo_detail.html', context)
+#     except Todo.DoesNotExist:
+#         raise Http404
+
 def todo_detail_view(request, id):
-    try:
-        todo = Todo.objects.get(pk=id)
-        context = dict(
-          todo=todo
+    todo = get_object_or_404(Todo, pk=id)
+    context = dict(
+       todo=todo,
           )
-        return render(request, 'todo/todo_detail.html', context)
-    except Todo.DoesNotExist:
-        raise Http404
+    return render(request, 'todo/todo_detail.html', context)
+
